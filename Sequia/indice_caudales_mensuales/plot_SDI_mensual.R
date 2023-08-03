1e060e97d427f5aaea5a9276f7c18e7701317847
Rafael Rodriguez Nuñez <78757629+rrod92@users.noreply.github.com>
2023-08-03 11:36:45 -0300

    Actualizacion SDI

--- Sequia/indice_caudales_mensuales/plot_SDI_mensual.R
+++ Sequia/indice_caudales_mensuales/plot_SDI_mensual.R
@@ -6,11 +6,12 @@ library(extrafont)
 loadfonts(device = "win")
 
 # Definir parametros de entrada
-name.station <- "paso roldan"
-codigo.station <- 117
+name.station <- "durazno"
+name.station_plot <- "Durazno"
+codigo.station <- "2206"
 scale.sdi <- 6
 
-filename <- paste0("output/",scale.sdi,"-month","_CompleteSDI_",gsub(" ","",name.station),".txt")
+filename <- paste0("output/",scale.sdi,"-month","_CompleteSDI_junio_",gsub(" ","",name.station),".txt")
 
 # leer el archvio de salida del codigo
 d <- read.csv(filename,sep = ",",header = TRUE)
@@ -18,6 +19,10 @@ d$Fecha <- as.Date(d$Fecha,format = "%Y-%m-%d")
 # agregar severidad de sequia en base a los valores de SDI mensual
 d$State_sdi <- cut(d$GammaSDI,c(10,0,-1,-1.5,-2,-10),labels = c("Sequía extrema","Sequía severa","Sequía moderada","Sequía leve","No sequía"),right=FALSE)
 # Plotear
+
+
+
+
 p <- ggplot(d,aes(x=Fecha,y=GammaSDI)) + 
   geom_bar(aes(fill=State_sdi), stat = "identity",colour = "black", na.rm = TRUE) +
   scale_fill_manual("Simbología",values = c("Sequía extrema" = "#730000",
@@ -28,12 +33,12 @@ p <- ggplot(d,aes(x=Fecha,y=GammaSDI)) +
   scale_x_date(date_breaks = "1 month", 
                labels = date_format("%b.%Y"),
                #limits = c(d$fecha_hasta[1],tail(d$fecha_hasta,n=1))) +
-               limits = c(as.Date("2021-01-01"),as.Date("2023-02-01"))) + 
+               limits = c(as.Date("2021-05-01"),as.Date("2023-07-01"))) + 
   scale_y_continuous(breaks = scales::pretty_breaks(n = 20),
-                     limits = c(-4,4)) +
+                     limits = c(-3,3)) +
   theme(axis.text.x = element_text(angle = 45, hjust = 1)) + 
   labs(title = "Indice Sequía en Caudales (SDI)",
-       subtitle = paste0("Estación:", name.station, " ", codigo.station, "- Escala temporal de ", scale.sdi, " Meses"),
+       subtitle = paste0("Estación:", name.station_plot, " cod: ", codigo.station, " - Escala temporal de ", scale.sdi, " Meses"),
        x = "Fecha",
        y = "SDI (-)", 
        caption = "Elaborado por: Jose Rodolfo Valles León")
@@ -54,7 +59,7 @@ p + theme(plot.title = element_text(size = 15,
           legend.text = element_text(size = 10,
                                      family = "Cambria"),
           legend.title.align = 0.5,
-          axis.title.x = element_text(size = 12,
+          axis.title.x = element_text(size = 10,
                                       face = "bold",
                                       family = "Cambria"),
           axis.title.y = element_text(size = 12,
@@ -62,3 +67,9 @@ p + theme(plot.title = element_text(size = 15,
                                       family = "Cambria"))
 
 
+
+dev.print(png,filename='output/durazno_SDI_jun2023_6M.png',
+    width = 1250 , height = 593, res=100)
+
+dev.off()
+ 
