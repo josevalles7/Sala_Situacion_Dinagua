cc1c690efb31c6f7d6647a664891f7b48f8a7a8e
Jose Valles <jose.valles.leon@gmail.com>
2025-04-02 13:14:24 -0300

    update files 2025-03

--- Status_Outlook_Bulletin/07_json2csv_waterbalance.bat
+++ Status_Outlook_Bulletin/07_json2csv_waterbalance.bat
@@ -1,8 +1,12 @@
-pause
+@echo off
+
+echo Ingrese la fecha correspondiente (YYYY-MM): 
+set /p filename_json=
+
 echo Convirtiendo json a csv 01-Mes
-c:/Users/DINAGUA/anaconda3/envs/HydroSOS/python.exe python_scripts/json_to_csv.py "./waterbalance/output_json/01_month" "2025-02" "qgis_status_outlook/csvtables" "01"
+c:/Users/DINAGUA/anaconda3/envs/HydroSOS/python.exe python_scripts/json_to_csv.py "./waterbalance/output_json/01_month" %filename_json% "qgis_status_outlook/csvtables" "01"
 echo Convirtiendo json a csv 03-Meses
-c:/Users/DINAGUA/anaconda3/envs/HydroSOS/python.exe python_scripts/json_to_csv.py "./waterbalance/output_json/03_month" "2025-02" "qgis_status_outlook/csvtables" "03"
+c:/Users/DINAGUA/anaconda3/envs/HydroSOS/python.exe python_scripts/json_to_csv.py "./waterbalance/output_json/03_month" %filename_json% "qgis_status_outlook/csvtables" "03"
 echo Convirtiendo json a csv 12-Meses
-c:/Users/DINAGUA/anaconda3/envs/HydroSOS/python.exe python_scripts/json_to_csv.py "./waterbalance/output_json/12_month" "2025-02" "qgis_status_outlook/csvtables" "12"
+c:/Users/DINAGUA/anaconda3/envs/HydroSOS/python.exe python_scripts/json_to_csv.py "./waterbalance/output_json/12_month" %filename_json% "qgis_status_outlook/csvtables" "12"
 pause
