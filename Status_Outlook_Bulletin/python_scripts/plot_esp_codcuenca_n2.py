f8e15d42e1696d44b5dab6eb408731421e7475f3
Jose Valles <jose.valles.leon@gmail.com>
2025-10-06 15:20:08 -0300

    update setiembre 2025

--- Status_Outlook_Bulletin/python_scripts/plot_esp_codcuenca_n2.py
+++ Status_Outlook_Bulletin/python_scripts/plot_esp_codcuenca_n2.py
@@ -443,8 +443,8 @@ ax3 = category_counts['percentage_ensemble'].plot(kind='bar',color=colors,figsiz
 plt.xticks(range(len(category_counts.index)),column_name,rotation=0,horizontalalignment='center');
 plt.xlabel('Categoria de Perspectiva Hidrologica',fontweight='bold',fontsize=14);
 plt.ylabel('% de miembros',fontweight='bold',fontsize=14);
-plt.suptitle(f'Ensemble Streamflow Prediction (ESP) en subcuenca: {codcuenca_n2}',fontweight='bold',fontsize=20);
-plt.title(f"{forecast_leadtime}-Mes de Perspectiva",fontweight='bold',fontsize=14);
+#plt.suptitle(f'Ensemble Streamflow Prediction (ESP) en subcuenca: {codcuenca_n2}',fontweight='bold',fontsize=20);
+#plt.title(f"{forecast_leadtime}-Mes de Perspectiva",fontweight='bold',fontsize=14);
 plt.savefig(f"./waterbalance/output_png/05_barplot_{forecast_leadtime}_leadtime.png", dpi=300, bbox_inches='tight')
 plt.close()
 
@@ -462,7 +462,7 @@ ax3 = category_counts_summary['percentage_ensemble'].plot(kind='bar',color=color
 plt.xticks(range(len(category_counts_summary.index)),column_name,rotation=0,horizontalalignment='center');
 plt.xlabel('Categoria de Perspectiva Hidrologica',fontweight='bold',fontsize=14);
 plt.ylabel('% de miembros',fontweight='bold',fontsize=14);
-plt.suptitle(f'Ensemble Streamflow Prediction (ESP) en subcuenca: {codcuenca_n2}',fontweight='bold',fontsize=20);
-plt.title(f"{forecast_leadtime}-Mes de Perspectiva",fontweight='bold',fontsize=14);
+#plt.suptitle(f'Ensemble Streamflow Prediction (ESP) en subcuenca: {codcuenca_n2}',fontweight='bold',fontsize=20);
+#plt.title(f"{forecast_leadtime}-Mes de Perspectiva",fontweight='bold',fontsize=14);
 plt.savefig(f"./waterbalance/output_png/06_barplot_{forecast_leadtime}_leadtime.png", dpi=300, bbox_inches='tight')
 plt.close()
